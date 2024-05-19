e772edc05955b43cfea4383a32213a3d2d999d74
Gustavo Meza <gustavo.meza@cit.hn>
2024-05-19 13:24:44 -0600

    feat: Grafico en linea como demo y cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as Highcharts from 'highcharts';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dashboard';
+
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    credits: {
+      enabled: false,
+    },
+    series: [{
+      data: [1, 2, 3],
+      type: 'line'
+    }]
+  };
+
+  cards = [
+    {
+      title: 'Total invoiced',
+      icon: '',
+      value: 88817.30
+    },
+    {
+      title: 'Average invoice',
+      icon: '',
+      value: 9868.59
+    },
+    {
+      title: 'Total orders',
+      icon: '',
+      value: 9
+    },
+    {
+      title: 'Total customers',
+      icon: '',
+      value: 10
+    },
+
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HighchartsChartModule,
     AppRoutingModule
   ],
   providers: [],
